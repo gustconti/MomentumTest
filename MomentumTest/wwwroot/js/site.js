3aca9e2a60bc8fd57392b23625b1f69c59539717
Gustavo Conti <gust.conti@gmail.com>
2024-10-10 05:56:14 -0300

    responsiveness, table icons and styles

--- MomentumTest/wwwroot/js/site.js
+++ MomentumTest/wwwroot/js/site.js
@@ -1,4 +1,42 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function debounce(func, wait) {
+    let timeout;
+    return function (...args) {
+        const context = this;
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(context, args), wait);
+    };
+}
 
-// Write your JavaScript code.
+function toggleMenu() {
+    $('#sidebar').toggleClass('closed');
+    $('#main-content').toggleClass('shifted');
+    $('#page-header').toggleClass('shifted');
+}
+
+$(document).ready(function () {
+    function handleResponsiveSidebar() {
+        var windowWidth = $(window).width();
+        var mobileBreakpoint = 768;
+        if (windowWidth < mobileBreakpoint) {
+            if (!$('#sidebar').hasClass('closed')) {
+                $('#sidebar').addClass('closed');
+                $('#main-content').addClass('shifted');
+                $('#page-header').addClass('shifted');
+            }
+        } else {
+            if ($('#sidebar').hasClass('closed')) {
+                $('#sidebar').removeClass('closed');
+                $('#main-content').removeClass('shifted');
+                $('#page-header').removeClass('shifted');
+            }
+        }
+    }
+
+    handleResponsiveSidebar();
+
+    $(window).resize(handleResponsiveSidebar);
+    
+    $('#toggle-button').on('click', function () {
+        toggleMenu();
+    });
+});
